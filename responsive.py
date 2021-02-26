0a336594f5bf19fa476b14f1ba7f9a7e3da37a05
Gangyong92 <hyeonggang@naver.com>
2021-02-26 16:48:58 +0900

    #2.1 Selenium
    ~#2.3 refactor

--- responsive.py
+++ responsive.py
@@ -3,28 +3,40 @@ import time
 from selenium import webdriver
 from webdriver_manager.chrome import ChromeDriverManager
 
-BROWSER_HEIGHT = 1079
+class ResponsiveTester:
 
-browser = webdriver.Chrome(ChromeDriverManager().install())
+    def __init__(self, urls):
+        self.browser = webdriver.Chrome(ChromeDriverManager().install())
+        self.browser.maximize_window()
+        self.urls = urls
+        self.sizes = [480, 960, 1366, 1920]
+        self.BROWSER_HEIGHT = 945
+    
+    def screenshot(self, url):
+        self.browser.get(url)
 
-browser.get("https://nomadcoders.co")
+        for size in self.sizes:
+            self.browser.set_window_size(size, 1079)
+            time.sleep(1)
+            # scroll 사이즈 javascript로 보내고 반환 받기
+            scroll_size = self.browser.execute_script("return document.body.scrollHeight")
+            # 총 스크롤 횟수 계산
+            total_sections = ceil(scroll_size / self.BROWSER_HEIGHT)
 
-# full screen으로 만들어줌. 모니터 여러개면 모든 모니터 다 사용해서 넓힘.
-browser.maximize_window()
+            # 스크롤 수행
+            for section in range(total_sections + 1):
+                self.browser.execute_script(f"window.scrollTo(0, {section * self.BROWSER_HEIGHT})")
+                time.sleep(2)
+                self.browser.save_screenshot(f"screenshots/{size}x{section}.png")
 
-# ms에 나오는 표준 윈도우 사이즈중 가로 길이들.
-sizes = [480, 960, 1366, 1920]
+    def start(self):
+        for url in self.urls:
+            self.screenshot(url)
 
-for size in sizes:
-    browser.set_window_size(size, 1079)
-    time.sleep(1)
-    # scroll 사이즈 javascript로 보내고 반환 받기
-    scroll_size = browser.execute_script("return document.body.scrollHeight")
-    # 총 스크롤 횟수 계산
-    total_sections = ceil(scroll_size / BROWSER_HEIGHT)
+    def finish(self):
+        self.browser.quit()
 
-    # 스크롤 수행
-    for section in range(total_sections + 1):
-        browser.execute_script(f"window.scrollTo(0, {section * BROWSER_HEIGHT})")
-        time.sleep(2)
-        browser.save_screenshot(f"screenshots/{size}x{section}.png")
+
+tester = ResponsiveTester(["https://nomadcoders.co"])
+tester.start()
+tester.finish()
